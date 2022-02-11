27401a583f2f01b4195753fb3df82e786145de5f
Physics_Cat <physicscat97@gmail.com>
2022-02-11 07:51:35 +0000

    second prototype with coloured letter display

--- play_wordle.py
+++ play_wordle.py
@@ -1,4 +1,7 @@
 from wordle import Wordle
+from colorama import Fore
+from letter_state import LetterState
+from typing import List
 
 def main():
     print("Test Wordle")
@@ -6,12 +9,43 @@ def main():
 
     while wordle.can_attempt:
         x=input("Type your guess: ")
+
+        if len(x) != wordle.Word_Length:
+            print(Fore.RED
+                  +f"Word must be {wordle.Word_Length} characters long!!"
+                  +Fore.RESET
+            )
+            continue
         wordle.attempt(x)
+        display_results(wordle)
+
 
     if wordle.is_solved:
         print("Congrats!! You found the correct answer")
     else:
         print("Game Over")
+
+def display_results(wordle: Wordle):
+    for word in wordle.attempts:
+        result=wordle.guess(word)
+        coloured_result_str=convert_results_to_colour(result)
+        print(coloured_result_str)
+    pass
+
+def convert_results_to_colour(result: List[LetterState]):
+    result_with_colour = []
+    for letter in result:
+        if letter.is_in_position:
+            colour=Fore.GREEN
+        elif letter.is_in_word:
+            colour=Fore.YELLOW
+        else:
+            colour=Fore.WHITE
+        coloured_letter=colour+letter.character+Fore.RESET
+        result_with_colour.append(coloured_letter)
+    return "".join(result_with_colour)
+
+
 
 
 if __name__ == "__main__":
--- wordle.py
+++ wordle.py
@@ -1,16 +1,30 @@
 #Logic file where I store my main code
+from letter_state import LetterState
 class Wordle:
 
     Max_Attempts=6
     Word_Length=5
 
     def __init__(self, secret: str):
-        self.secret: str=secret
+        self.secret: str=secret.upper()
         self.attempts=[]
-        pass
+#        pass
 
     def attempt(self, word:str):
+        word=word.upper()
         self.attempts.append(word)
+
+    def guess(self, word:str):
+        word=word.upper()
+        result = []
+
+        for i in range(self.Word_Length):
+            character=word[i]
+            letter=LetterState(character)
+            letter.is_in_word = character in self.secret
+            letter.is_in_position=character==self.secret[i]
+            result.append(letter)
+        return result
 
 
     @property
